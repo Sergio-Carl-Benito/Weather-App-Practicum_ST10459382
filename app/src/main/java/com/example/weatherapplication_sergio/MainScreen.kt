2e65b2b0997fb7aa9a98bed08c254a3d7e6b21a7
ST10459382-Sergio-Benito <163974756+Sergio-Carl-Benito@users.noreply.github.com>
2024-06-10 10:48:03 +0200

    added populated Arrays, added logic to calculate average temperature, displayed average temperature

--- app/src/main/java/com/example/weatherapplication_sergio/MainScreen.kt
+++ app/src/main/java/com/example/weatherapplication_sergio/MainScreen.kt
@@ -6,9 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 
 class MainScreen : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
@@ -16,6 +17,27 @@ class MainScreen : AppCompatActivity() {
         // Declaring variables and connecting frontend elements to backend code
         val navigate_DailyTemperatures = findViewById<Button>(R.id.navigate_MainScreen)
         val navigate_Exit_App = findViewById<Button>(R.id.navigate_Exit_App)
+
+        // Array of Max temperatures
+        val Temp_Max = arrayOf(5,6,4,5,7,6,5)
+        // Array of Min temperatures
+        val Temp_Min = arrayOf(17,18,16,19,20,18,17)
+        // Array of Strings (first set)
+        val Days_Of_Week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+        // Array of Strings (second set)
+        val Weather_Condition = arrayOf("Foggy", "Overcast", "Heavy Rain", "Sunny", "Sunny", "Overcast", "Light Rain")
+
+        // Calculate the total of max and min temperatures
+        val totalTempMax = Temp_Max.sum()
+        val totalTempMin = Temp_Min.sum()
+
+        // Calculate the average temperature
+        val averageTemp = (totalTempMax + totalTempMin) / (Temp_Max.size + Temp_Min.size)
+
+        val Text_AverageTemp = findViewById<EditText>(R.id.Text_AverageTemp)
+
+        // Display the average temperature in the EditText
+        Text_AverageTemp.setText("$averageTemp°C")
 
         // Set click listener for the start button
         navigate_DailyTemperatures.setOnClickListener {
